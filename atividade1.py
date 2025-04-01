feb670e02781ca6074788f242f7dd36047c50e9c
mariane <mariane.assis.bezerra@gmail.com>
2025-04-01 14:42:15 -0300

    salário

--- atividade1.py
+++ atividade1.py
@@ -1,5 +1,9 @@
 nome = input("digite seu nome")
 idade = int(input("digite sua idade"))
 salario = float(input("digite seu salário"))
+percentual = float (input("digite o percentual"))
 
-print(f"ola seu nome {nome} sua {idade}, seu salaário {salario}")
+valorreal = salario *  percentual/100
+novosalario = salario + valorreal
+
+print(f"ola seu nome {nome} sua {idade}, seu novo salário {novosalario}")
